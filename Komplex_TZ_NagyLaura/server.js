2f87cdbfd3eddecbb8ef677c163ac6ec1510b999
NagLau <naglau747@hengersor.hu>
2025-03-10 13:46:50 +0100

    TÉMAZÁRÓ-2025.03.10.
    Szerver futtatása

---
+++ Komplex_TZ_NagyLaura/server.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const app = express()
+const mysql = require("mysql")
+const cors = require("cors")
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(cors());
+ 
+const db = mysql.createConnection({
+    user:"root",
+    host:"127.0.0.1",
+    port: 3307,
+    password:"",
+    database:""
+})
+ 
+app.get("/",(req,res)=>
+{
+    res.send("Fut a Backend")
+})
+ 
+app.get ("/",( req, res) => {
+    const sql ="SELECT * FROM ``";
+    db.query(sql, (err,result) => {
+        if (err) return res.json(err);
+        return res.json(result)
+    })
+})
+ 
+app.listen(3059, ()=>
+{
+    console.log("A szerverem a 3059 porton fut")
+})
+
+
