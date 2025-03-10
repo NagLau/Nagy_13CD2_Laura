4386d6087127b7317adddfa1b4788b6bc4360d46
NagLau <naglau747@hengersor.hu>
2025-03-10 14:47:42 +0100

    TÉMAZÁRÓ_VÉGLEGESÍTÉS
    azt hittem mégsem adom fel, de mégis :c

--- Komplex_TZ_NagyLaura/server.js
+++ Komplex_TZ_NagyLaura/server.js
@@ -43,6 +43,30 @@ app.get("/agazat", (req, res) => {
     });
 });
 
+app.get("/lekerdezes_", (req, res) => {
+    const sql = `SELECT d.nev, t.agazat, (d.hozott + d.kpmagy + d.kpmat) AS osszpont FROM felveteli.jelentkezesek j JOIN felveteli.diakok d ON j.diak = d.oktazon JOIN felveteli.tagozatok t ON j.tag = t.akod ORDER BY d.nev ASC;`;
+ 
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.error("SQL Hiba:", err);
+            return res.status(500).json({ error: "Adatbázis lekérdezési hiba!" });
+        }
+        return res.json(result);
+    });
+});
+ 
+app.get("/lekerdezes__", (req, res) => {
+    const sql = `SELECT t.agazat, COUNT(j.diak) AS jelentkezok_szama, SUM(d.hozott + d.kpmagy + d.kpmat) AS osszpontszam FROM felveteli.jelentkezesek j JOIN felveteli.diakok d ON j.diak = d.oktazon JOIN felveteli.tagozatok t ON j.tag = t.akod WHERE t.nyek = 1 GROUP BY t.agazat ORDER BY jelentkezok_szama DESC;`;
+ 
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.error("SQL Hiba:", err);
+            return res.status(500).json({ error: "Adatbázis lekérdezési hiba!" });
+        }
+        return res.json(result);
+    });
+});
+
 app.listen(3059, ()=>
 {
     console.log("A szerverem a 3059 porton fut")
